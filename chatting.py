97f03c106202166598f3fad47d0fb27e92b3b86d
John Sag <johnsag@Johns-MacBook-Pro-2.local>
2020-09-22 22:43:28 -0400

    added if statement
    for a user response we return a string from the list

--- chatting.py
+++ chatting.py
@@ -1,6 +1,16 @@
 from random import choice
 def get_box_response(user_response):
 
-      bot_response_hot = ["wear shorts", "Wear something light","wear a t-shirt"]
+    bot_response_hot = ["wear shorts", "Wear something light","wear a t-shirt"]
     bot_response_cold = ["wear a coat", "wear a scarf", "wear a hat"]
     bot_response_chilly = ["wear a hoodie", "wear a beanie", "wear a thick flanel"]
+
+     if user_response == "hot":
+        return choice(bot_response_hot)
+    elif user_response == "cold":
+        return choice(bot_response_cold)
+    elif user_response == "chilly":
+        return choice(bot_response_chilly)
+    else:
+        return "I'm not sure what you should wear"
+
