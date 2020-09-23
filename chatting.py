dd488c16a937c11863ed0d22fdaefca39a700893
John Sag <johnsag@Johns-MacBook-Pro-2.local>
2020-09-22 22:42:00 -0400

    Added lists
    Added lists of strings for the bot responses

--- chatting.py
+++ chatting.py
@@ -1,3 +1,6 @@
 from random import choice
 def get_box_response(user_response):
-    
+
+      bot_response_hot = ["wear shorts", "Wear something light","wear a t-shirt"]
+    bot_response_cold = ["wear a coat", "wear a scarf", "wear a hat"]
+    bot_response_chilly = ["wear a hoodie", "wear a beanie", "wear a thick flanel"]
